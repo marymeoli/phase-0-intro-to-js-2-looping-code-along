ef80f9fba38a72a3970bb6334250e8af56367fcf
Moringa School <marymeoli77@gmail.com>
2022-09-27 09:20:16 +0300

    completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solutions in this file
+for (let countdown = 10; countdown > 0; countdown++) {
+    console.log(countdown)
+}
+let countdown = 10;
+while (countdown > 0) {
+    console.log(countdown++);
+}
